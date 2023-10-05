6ad0a2d5bedb0f51187e09cf3d221dd726e7e91d
Sergei Anisimov <s.anisimov.it@gmail.com>
2023-10-05 16:48:36 +0200

    Update MapGenerator.cs

--- Assets/CodeBase/Generators/MapGenerator/MapGenerator.cs
+++ Assets/CodeBase/Generators/MapGenerator/MapGenerator.cs
@@ -16,8 +16,8 @@ namespace CodeBase.Generators.MapGenerator
       var map = new MapChunk[_width, _lenght];
 
       for (int x = 0; x < _width; x++)
-      for (int y = 0; y < _lenght; y++)
-        map[x, y] = new MapChunk(x, y);
+        for (int y = 0; y < _lenght; y++)
+          map[x, y] = new MapChunk(x, y);
 
       return map;
     }
